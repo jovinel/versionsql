f6b17cb21897cb1bde58dc705d3a8778f57c8272
Jovi Nel Parilla <jnp.parilla@gmail.com>
2022-05-02 14:13:41 +0800

    Add m_products table

---
+++ VersionSQL/RSD-JOVI/SQLEXPRESS2019/SalesPromotionExpensesDB/dbo/Table/m_products.sql
@@ -0,0 +1,9 @@
+﻿/****** Object:  Table [dbo].[m_products]    Committed by VersionSQL https://www.versionsql.com ******/
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+CREATE TABLE [dbo].[m_products](
+	[id] [bigint] NOT NULL,
+	[name] [nchar](150) NOT NULL,
+	[price] [decimal](18, 0) NOT NULL
+) ON [PRIMARY]
