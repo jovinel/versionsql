c26e4501719f5a0df85d6841978143aee1299015
Jovi Nel Parilla <jnp.parilla@gmail.com>
2022-05-02 14:25:21 +0800

    Add Persons table Drop price column on m_products

---
+++ VersionSQL/RSD-JOVI/SQLEXPRESS2019/SalesPromotionExpensesDB/dbo/Table/Persons.sql
@@ -0,0 +1,11 @@
+﻿/****** Object:  Table [dbo].[Persons]    Committed by VersionSQL https://www.versionsql.com ******/
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+CREATE TABLE [dbo].[Persons](
+	[PersonID] [int] NULL,
+	[LastName] [varchar](255) NULL,
+	[FirstName] [varchar](255) NULL,
+	[Address] [varchar](255) NULL,
+	[City] [varchar](255) NULL
+) ON [PRIMARY]
--- VersionSQL/RSD-JOVI/SQLEXPRESS2019/SalesPromotionExpensesDB/dbo/Table/m_products.sql
+++ VersionSQL/RSD-JOVI/SQLEXPRESS2019/SalesPromotionExpensesDB/dbo/Table/m_products.sql
@@ -4,6 +4,5 @@ SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
 CREATE TABLE [dbo].[m_products](
 	[id] [bigint] NOT NULL,
-	[name] [nchar](150) NOT NULL,
-	[price] [decimal](18, 0) NOT NULL
+	[name] [nchar](150) NOT NULL
 ) ON [PRIMARY]
