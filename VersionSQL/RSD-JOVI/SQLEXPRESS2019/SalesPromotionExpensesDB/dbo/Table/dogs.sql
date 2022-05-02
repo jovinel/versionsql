e9de414ca23812d2bb91716c6e09de5b7b6a76a8
Jovi Nel Parilla <jnp.parilla@gmail.com>
2022-05-02 14:46:08 +0800

    Added dogs table

---
+++ VersionSQL/RSD-JOVI/SQLEXPRESS2019/SalesPromotionExpensesDB/dbo/Table/dogs.sql
@@ -0,0 +1,9 @@
+﻿/****** Object:  Table [dbo].[dogs]    Committed by VersionSQL https://www.versionsql.com ******/
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+CREATE TABLE [dbo].[dogs](
+	[id] [bigint] NULL,
+	[name] [nchar](10) NULL,
+	[location] [nchar](10) NULL
+) ON [PRIMARY]
