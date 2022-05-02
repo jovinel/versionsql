4e5aaa63c894c924ca2e9627a8a8c39db74501eb
Jovi <jovi_nel_parilla@ring-group.jp>
2022-05-02 14:11:17 +0800

    Initial Commit

---
+++ VersionSQL/RSD-JOVI/SQLEXPRESS2019/SalesPromotionExpensesDB/dbo/Table/Users.sql
@@ -0,0 +1,13 @@
+﻿/****** Object:  Table [dbo].[Users]    Committed by VersionSQL https://www.versionsql.com ******/
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+CREATE TABLE [dbo].[Users](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](max) NULL,
+	[address] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
---
+++ VersionSQL/RSD-JOVI/SQLEXPRESS2019/SalesPromotionExpensesDB/dbo/View/UsersView.sql
@@ -0,0 +1,3 @@
+﻿/****** Object:  View [dbo].[UsersView]    Committed by VersionSQL https://www.versionsql.com ******/
+
+CREATE VIEW UsersView AS SELECT * FROM Users;
